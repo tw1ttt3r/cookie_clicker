35c7cfdce3ef07e2e0fd1c24f6f2a9f5ec296256
tw1ttt3r <tw1ttt3r@gmail.com>
2026-03-19 10:40:47 -0600

    feat: added intern container

--- src/components/mainWrapper/MainWrapper.tsx
+++ src/components/mainWrapper/MainWrapper.tsx
@@ -7,9 +7,13 @@ type MainWrapperType = {
 
 function MainWrapper({ children }: MainWrapperType) {
   return (
-    <main className="w-screen p-3 h-dvh rounded relative">
-      { children }
-      <Version />
+    <main className="w-dvw px-3 h-dvh rounded relative flex justify-center">
+      <section
+        className="mobile:min-w-130 mobile:max-w-140 tablet:min-w-160 tablet:max-w-170 desktop:max-w-270 desktop:min-w-260 xxl:max-w-300 xxl:mix-w-290 border bg-amber-600"
+      >
+        { children }
+        <Version />
+      </section>
     </main>
   )
 }
