bebfea69e61ad05b86cd5c88a8201c95b989fba1
tw1ttt3r <tw1ttt3r@gmail.com>
2026-03-19 19:21:18 -0600

    feat: use context

--- src/components/Cookie/Cookie.tsx
+++ src/components/Cookie/Cookie.tsx
@@ -1,9 +1,17 @@
+import { useCookieContext } from "@_config/hooks/useCookieContext";
+
 function Cookie() {
+
+  const { registerAccumulated } = useCookieContext()
+
+  const handlerClic = () => registerAccumulated()
+
   return(
     <div className="cookieWrapper">
       <img
+        onClick={handlerClic}
         src="./cookie.png"
-        className="mobile:w-[65%] tablet:w-[60%] desktop:w-[50%] cookie-spin"
+        className="mobile:w-[65%] tablet:w-[60%] desktop:w-[50%] cookie-spin cursor-pointer"
       />
     </div>
   )
