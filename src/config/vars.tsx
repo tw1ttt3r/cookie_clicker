ce58b098c2f978ab5622cc3e35a6c0ba922324f9
tw1ttt3r <tw1ttt3r@gmail.com>
2026-03-23 17:45:30 -0600

    feat: register evn vars

--- src/config/vars.tsx
+++ src/config/vars.tsx
@@ -1,8 +1,18 @@
 import { version } from "@/package.json"
 
 const local_storage = import.meta.env.VITE_COOKIE_DATA
+const filename = import.meta.env.VITE_FILENAME
+const filetype = import.meta.env.VITE_FILETYPE
+const filetagtype = import.meta.env.VITE_FILE_TAG_TYPE
+const filesupporttype = import.meta.env.VITE_FILE_SUPPORT_TYPE
+const filetag = import.meta.env.VITE_FILE_TAG
 
 export {
   version,
-  local_storage
+  local_storage,
+  filename,
+  filetype,
+  filetagtype,
+  filesupporttype,
+  filetag
 }
